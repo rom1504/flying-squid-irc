4fdd8e991c09da6bd3f35548a9c5bd4e3dda4c07
Romain Beaumont <romain.rom1@gmail.com>
2015-11-24 10:17:15 +0100

    improve code a bit : self->this

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
 var irc = require('irc');
 
 module.exports.server=function(serv){
-  var self=this;
-
-  var client=serv.irc=new irc.Client(self.settings.server, self.settings.nick, {
-    channels: [self.settings.chan],
-    password: self.settings.pass,
+  this.irc=new irc.Client(this.settings.server, this.settings.nick, {
+    channels: [this.settings.chan],
+    password: this.settings.pass,
     secure:true
   });
-  this.irc=client;
-
-
-  client.nick=self.settings.nick;
-
-  client._updateMaxLineLength();
-
-  client.addListener('message', function (from, to, message) {
+  this.irc.nick=this.settings.nick;
+  this.irc._updateMaxLineLength();
+  this.irc.addListener('message', function (from, to, message) {
     serv.broadcast("[irc] "+from+": "+message);
   });
-
-  if(self.settings.startingMessage) client.say(self.settings.chan, self.settings.startingMessage);
-
-  client.addListener('error', function(message) {
+  if(this.settings.startingMessage) this.irc.say(this.settings.chan, this.settings.startingMessage);
+  this.irc.addListener('error', function(message) {
     console.log('error: ', message);
   });
 };
@@ -29,9 +20,8 @@ module.exports.server=function(serv){
 module.exports.player=function(player,serv)
 {
   var self=this;
-  var client=self.irc;
    player.on('chat',function(message){
-     client.say(self.settings.chan, "[minecraft] "+player.username+": "+message);
+     self.irc.say(self.settings.chan, "[mc] "+player.username+": "+message);
    })
 };
 
